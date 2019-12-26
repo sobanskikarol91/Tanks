10dcfa08a278010804dbe434ce2a1d97c81732c9
Karol Sobański <sobanskikarol91@gmail.com>
2019-12-27 00:04:45 +0100

    Add: Photon + Sprites

---
+++ Tanks/Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class NetworkManager : MonoBehaviourPunCallbacks  
+{
+    private void Start()
+    {
+        Debug.Log("Start");
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        PhotonNetwork.JoinRandomRoom();
+        PhotonNetwork.AutomaticallySyncScene = true;
+        Debug.Log("Connected do server");
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        Debug.Log("Failed to join a room");
+        CreateRoom();
+    }
+
+    private void CreateRoom()
+    {
+        RoomOptions options = new RoomOptions() { IsVisible = true, IsOpen = true, MaxPlayers = 2 };
+        PhotonNetwork.CreateRoom("Room", options);
+        Debug.Log("Creating room");
+    }
+
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        Debug.Log("Failed To Create room");
+    }
+
+
+}
---
+++ Tanks/Assets/Scripts/RoomManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using Photon.Pun;
+using System.IO;
+
+public class RoomManager : MonoBehaviourPunCallbacks 
+{
+    public override void OnEnable()
+    {
+        PhotonNetwork.AddCallbackTarget(this);
+    }
+
+    public override void OnDisable()
+    {
+        PhotonNetwork.RemoveCallbackTarget(this);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("Start game");
+        PhotonNetwork.Instantiate(Path.Combine("Prefabs", "NetworkPlayer"), Vector2.zero, Quaternion.identity);
+    }
+}
