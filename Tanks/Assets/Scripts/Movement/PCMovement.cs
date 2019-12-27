99d245a2be03b1f1ca5a90015007433eaf317387
Karol Sobański <sobanskikarol91@gmail.com>
2019-12-27 13:36:24 +0100

    [Map] Update

--- Tanks/Assets/Scripts/Movement/PCMovement.cs
+++ Tanks/Assets/Scripts/Movement/PCMovement.cs
@@ -4,11 +4,19 @@ public class PCMovement : Ability
 {
     [SerializeField] float speed = 1f;
 
+    private Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
     public void Execute()
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
-        transform.position += new Vector3(horizontal, vertical).normalized * speed * Time.deltaTime;
+        rb.position += new Vector2(horizontal, vertical).normalized * speed * Time.deltaTime;
+        
     }
 }
