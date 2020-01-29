c8b9781ed4c210571688ab6591ac317c9300fbc5
Karol Sobański <sobanskikarol91@gmail.com>
2020-01-29 21:11:12 +0100

    [Ammo] Add: snd

--- Tanks/Assets/Scripts/PowerUps/PowerUp.cs
+++ Tanks/Assets/Scripts/PowerUps/PowerUp.cs
@@ -1,14 +1,19 @@
 ﻿using Photon.Pun;
 using UnityEngine;
 
-public abstract class PowerUp : MonoBehaviour
+public abstract class PowerUp : MonoBehaviourPun
 {
+    [SerializeField] AudioClip pickUpSnd;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         Tank tank = collision.gameObject.GetComponent<Tank>();
 
         if (PhotonNetwork.IsMasterClient)
         {
+            if (photonView.IsMine)
+                AudioSource.PlayClipAtPoint(pickUpSnd, transform.position);
+
             CollectedByPlayer(collision);
             PhotonNetwork.Destroy(gameObject);
         }
